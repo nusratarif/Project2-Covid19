96f18f5dcd566dd0d3e3e793cdc31b4db0ea822a
nusratarif <57041637+nusratarif@users.noreply.github.com>
2021-01-09 20:56:19 +0500

    Add files via upload

---
+++ precache-manifest.fd339f53242eec296ffb91528acfc27a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770d7b82e521ce2f4f39815a76c1450e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ea5cbe780ba3a16edfc",
+    "url": "/static/css/main.f9717d36.chunk.css"
+  },
+  {
+    "revision": "5572b7d7e1e00fe29be6",
+    "url": "/static/js/2.9ddbe258.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.9ddbe258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea5cbe780ba3a16edfc",
+    "url": "/static/js/main.8932d1e0.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  }
+]);
